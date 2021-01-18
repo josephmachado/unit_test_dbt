24eec43c45d95b667fcd0fdd4c4bb2bd60e0db0e
josephmachado <joseph.machado.professional@gmail.com>
2021-01-18 11:23:13 -0500

    dbt run script

---
+++ run_dbt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+env=$1
+export DBT_PROFILES_DIR=$PWD
+if [[ $env == "prod" ]]; then
+    dbt debug --target prod 
+    dbt run --target prod 
+    dbt test --target prod 
+    dbt docs generate --target prod 
+    dbt docs serve --target prod 
+else
+    dbt seed --full-refresh 
+    dbt run 
+    dbt test 
+    dbt docs generate 
+    dbt docs serve 
+fi
